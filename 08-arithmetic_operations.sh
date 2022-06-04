f63d4326dc5beb66403830b98bb3994eba60f09f
aganaedwin <99971121+aganaedwin@users.noreply.github.com>
2022-06-03 18:47:40 -0600

    Update 08-arithmetic_operations.sh

--- 08-arithmetic_operations.sh
+++ 08-arithmetic_operations.sh
@@ -1,10 +1,10 @@
-#!/bin/bash      
+#!/bin/bash 
+# back tick ` `
 #title           : Arithmetic operations
 
 expr 3 + 2
 expr 3 - 2
-expr 3 \* 2
-expr 10 / 2
-expr 20 % 3
+expr 3 \* 2    # multiplication
+expr 10 / 2    # division
+expr 20 % 3    # remainder
 echo addition of 3 and 2 is : `expr 3 + 2`
-
